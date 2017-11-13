09f0df8bdd52dd97b6a25b5c856612713bb099d0
shalinee.singh <shalinee.singh@coviam.com>
2017-11-13 13:01:59 +0530

    added connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname + '/../public');
 const PORT = process.env.PORT || 3000;
 
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+	console.log('new connection added');
+	socket.on('disconnect', () =>{
+		console.log("User disconnected");
+	})
+});
 app.use(express.static(publicPath));	
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
